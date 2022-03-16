8a4dbc2fe1321114163e747a4ffce6fdd8e99909
Luis Archer <luis.archer@ritain.io>
2022-03-16 11:02:53 +0000

    Modifications done

---
+++ README.md
@@ -0,0 +1,4 @@
+#Repository
+---
+This repository contains all the modules used by Terraform to deploy resources to the azure cloud environment.
+All modules added to this repository must follow CVTs respectrive formatting.
---
+++ vm/variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+  description = "Name of VM"
+  type        = string
+  default     = "VPC"
+}
+
+variable "region" {
+  description = "Region of Resource"
+  type        = string
+  default     = "VPC"
+}
