5da70538f8b67dd82b0a6bfdcad07a81070a5eda
Luis Archer <luis.archer@ritain.io>
2022-03-22 12:11:47 +0000

    Modifications done

--- actions/pre-commit/src/main.sh
+++ actions/pre-commit/src/main.sh
@@ -10,6 +10,7 @@ function main {
   pre-commit run -a | sed 's/\x1b\[[0-9;]*m//g' | tee comment_pre-commit.md
   base64 comment_pre-commit.md > b64encfile.md
   echo ::set-output name=filecontent::$(cat b64encfile.md)
+  echo ::debug::$(pwd)
   #echo ::set-output name=filecontent::$(cat comment_pre-commit.md)
 }
 
